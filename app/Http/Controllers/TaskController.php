fba89215893d73d56bf1493ac3701bf2c4cfcfba
koreoxy <koreoxy@koreoxy>
2025-07-03 11:59:03 +0700

    add : fitur tasks handle create

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Task;
+use App\Models\Employee;
 
 class TaskController extends Controller
 {
@@ -12,5 +13,29 @@ class TaskController extends Controller
         $tasks = Task::all();
 
         return view('tasks.index', compact('tasks'));
+    }
+
+    public function create() {
+
+        $employees = Employee::all();
+
+        return view('tasks.create', compact('employees'));
+    }
+
+    public function store(Request $request) {
+
+        // CEK VALIDASI TERLEBIH DAHULU
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'assigned_to' => 'required',
+            'due_date' => 'required|date',
+            'status' => 'required|string',
+        ]);
+
+        // JIKA BERHASIL
+        Task::create($validated);
+
+        return redirect()->route('tasks.index')->with('success', 'Task created successfully.');
     }
 }
