de7923a8e205e25a77088faa99811c8aed71c13b
koreoxy <koreoxy@koreoxy>
2025-07-04 14:40:27 +0700

    add : fitur taks - handle form edit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -38,4 +38,28 @@ class TaskController extends Controller
 
         return redirect()->route('tasks.index')->with('success', 'Task created successfully.');
     }
+    
+    public function edit(Task $task) {
+        $employees =  Employee::all();
+
+        return view('tasks.edit', compact('task', 'employees'));
+    }
+
+    
+    public function update(Request $request, Task $task) {
+
+        // CEK VALIDASI TERLEBIH DAHULU
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'assigned_to' => 'required',
+            'due_date' => 'required|date',
+            'status' => 'required|string',
+        ]);
+
+        // JIKA BERHASIL validasi maka update data
+        $task->update($validated);
+
+        return redirect()->route('tasks.index')->with('success', 'Task updated successfully.');
+    }
 }
