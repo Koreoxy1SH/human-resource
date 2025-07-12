c76901e8a194bf32b9287d3e516abbeb9373c142
Koreoxy <rayssankn@gmail.com>
2025-07-12 21:18:21 +0700

    add: fitur departments - create

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -12,4 +12,21 @@ class DepartmentController extends Controller
 
         return view('departments.index', compact('departments'));
     }
+
+    public function create() {
+
+        return view('departments.create');
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'required|string',
+        ]);
+
+        Department::create($request->all());
+
+        return redirect()->route('departments.index')->with('success', 'Departments create successfully');
+    }
 }
